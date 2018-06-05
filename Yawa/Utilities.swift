c6d7f30d9abfa05d725147b36cedfaeb25235602
Anton Vronskii <vronskii.anton@gmail.com>
2018-06-05 21:24:04 +0900

    Controls on adding transaction now look closer to mockups

--- Yawa/Utilities.swift
+++ Yawa/Utilities.swift
@@ -22,3 +22,22 @@ enum Currency {
 func formatMoney(amount: Float, currency: Currency, symbolEnabled: Bool = true) -> String {
   return NSString(format: "%@%.0f", symbolEnabled ? "¥" : "", amount) as String
 }
+
+extension UIButton {
+  func setBackgroundColor(color: UIColor, forState: UIControlState) {
+    UIGraphicsBeginImageContext(CGSize(width: 1, height: 1))
+    UIGraphicsGetCurrentContext()!.setFillColor(color.cgColor)
+    UIGraphicsGetCurrentContext()!.fill(CGRect(x: 0, y: 0, width: 1, height: 1))
+    let colorImage = UIGraphicsGetImageFromCurrentImageContext()
+    UIGraphicsEndImageContext()
+    self.setBackgroundImage(colorImage, for: forState)
+    
+//    UIGraphicsBeginImageContext(frame.size)
+//    UIGraphicsGetCurrentContext()!.setFillColor(color.cgColor)
+//    UIGraphicsGetCurrentContext()!.fill(bounds)
+//    UIBezierPath(roundedRect: bounds, cornerRadius: layer.cornerRadius).addClip()
+//    let colorImage = UIGraphicsGetImageFromCurrentImageContext()
+//    UIGraphicsEndImageContext()
+//    self.setBackgroundImage(colorImage, for: forState)
+  }
+}
