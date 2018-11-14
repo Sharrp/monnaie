0f82bc00efc1ed82ff6f063e058a000f324ac727
Anton Vronskii <vronskii.anton@gmail.com>
2018-11-15 07:24:55 +0900

    Move history & summary - related classes into a separate directory

---
+++ Yawa/UI/List & Summary/CellEditOrchestrator.swift
@@ -0,0 +1,21 @@
+//
+//  CellEditOrchestrator.swift
+//  Yawa
+//
+//  Created by Anton Vronskii on 2018/11/15.
+//  Copyright © 2018 Anton Vronskii. All rights reserved.
+//
+
+import UIKit
+
+struct CellEditOrchestrator {
+  private let composer: TransactionComposerView
+  
+  init(composer: TransactionComposerView) {
+    self.composer = composer
+  }
+  
+  func transitionToEdit(cell: TransactionCell) {
+    
+  }
+}
